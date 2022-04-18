0323674153ddedfdfed8e6a80e149bf4f722aded
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-04-18 14:17:48 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 odpovedi = [65, 66, 67, 68]
+radio.set_group(69)
+stop = 1
+
 
 def on_button_pressed_a():
-    radio.send_value("answer", odpovedi[0])
-    radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    global stop
+    if stop == 1:
+        radio.send_value("answer", odpovedi[0])
+        radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+        stop -= 1
+        basic.show_icon(IconNames.YES)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    radio.send_value("answer", odpovedi[1])
-    radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
-input.on_button_pressed(Button.A, on_button_pressed_b)
+    global stop
+    if stop == 1:
+        radio.send_value("answer", odpovedi[1])
+        radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+        stop -= 1
+        basic.show_icon(IconNames.YES)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_pin_released_p0():
-    radio.send_value("answer", odpovedi[2])
-    radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    global stop
+    if stop == 1:
+        radio.send_value("answer", odpovedi[2])
+        radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+        stop -= 1
+        basic.show_icon(IconNames.YES)
 input.on_pin_released(TouchPin.P0, on_pin_released_p0)
 
 def on_pin_released_p1():
-    radio.send_value("answer", odpovedi[3])
-    radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
-input.on_pin_released(TouchPin.P0, on_pin_released_p1)
+    global stop
+    if stop == 1:
+        radio.send_value("answer", odpovedi[3])
+        radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+        stop -= 1
+        basic.show_icon(IconNames.YES)
+input.on_pin_released(TouchPin.P1, on_pin_released_p1)
--- main.ts
+++ main.ts
@@ -1,17 +1,43 @@
 let odpovedi = [65, 66, 67, 68]
+radio.setGroup(69)
+let stop = 1
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendValue("answer", odpovedi[0])
-    radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    
+    if (stop == 1) {
+        radio.sendValue("answer", odpovedi[0])
+        radio.receivedPacket(RadioPacketProperty.SerialNumber)
+        stop -= 1
+        basic.showIcon(IconNames.Yes)
+    }
+    
 })
-input.onButtonPressed(Button.A, function on_button_pressed_b() {
-    radio.sendValue("answer", odpovedi[1])
-    radio.receivedPacket(RadioPacketProperty.SerialNumber)
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (stop == 1) {
+        radio.sendValue("answer", odpovedi[1])
+        radio.receivedPacket(RadioPacketProperty.SerialNumber)
+        stop -= 1
+        basic.showIcon(IconNames.Yes)
+    }
+    
 })
 input.onPinReleased(TouchPin.P0, function on_pin_released_p0() {
-    radio.sendValue("answer", odpovedi[2])
-    radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    
+    if (stop == 1) {
+        radio.sendValue("answer", odpovedi[2])
+        radio.receivedPacket(RadioPacketProperty.SerialNumber)
+        stop -= 1
+        basic.showIcon(IconNames.Yes)
+    }
+    
 })
-input.onPinReleased(TouchPin.P0, function on_pin_released_p1() {
-    radio.sendValue("answer", odpovedi[3])
-    radio.receivedPacket(RadioPacketProperty.SerialNumber)
+input.onPinReleased(TouchPin.P1, function on_pin_released_p1() {
+    
+    if (stop == 1) {
+        radio.sendValue("answer", odpovedi[3])
+        radio.receivedPacket(RadioPacketProperty.SerialNumber)
+        stop -= 1
+        basic.showIcon(IconNames.Yes)
+    }
+    
 })
